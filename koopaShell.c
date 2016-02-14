df90eb624c50fa5129605fc35c7a6c9ccc019432
Keiko Fujii <kfujii12@georgefox.edu>
2016-02-14 15:42:06 -0800

    Can now do all the cd things (no error checking)

--- koopaShell.c
+++ koopaShell.c
@@ -4,25 +4,52 @@
 #include <stdlib.h>
 
 #define MAX_SIZE 1024
+#define MAX_COMMAND_ARGS 256
+#define OVERWRITE 1
+#define BACKGROUND_COMMAND 1
+#define NO_BACKGROUND_COMMAND 0
 
 int main(int argc, char* argv[])
 {
+    //TODO: Errors
     /* Variable declarations */
     char command[MAX_SIZE];
-    char* commandArgs[256]; /* Max number */
+    char* commandArgs[MAX_COMMAND_ARGS]; /* Max number */
     int childPid = 0;
     int status;
+    char currentDirectory[MAX_SIZE];
+    char previousDirectory[MAX_SIZE];
+
+    // Assume that there is no background command for now
+    int backgroundCommand = NO_BACKGROUND_COMMAND;
+
+    getcwd(previousDirectory, MAX_SIZE);
 
     while(1) /* Repeat forever */
     {
+        getcwd(currentDirectory, MAX_SIZE);
+
         /* Display a prompt */
-        printf("KoopaShell> %% ");
+        printf("%s> %% ", currentDirectory);
 
         /* Read a command from the user */
         fgets(command, MAX_SIZE, stdin);
 
         /* Get rid of the new line character */
         command[strlen(command) - 1] = '\0';
+
+        // Check to see if there is a & signifying a command 
+        // running in the background
+        // if(!strcmp(&command[strlen(command) - 1], "&"))
+        // {
+        //     backgroundCommand = BACKGROUND_COMMAND;
+
+        //     // We know the command is meant to run in the background
+        //     // Get rid of the & so that we can make a commandArgs 
+        //     // without it 
+        //     command[strlen(command) - 1] = '\0';
+        // }
+
 
         /* Parse the string into the command portion and
          * an array of pointers to argument strings using the blank
@@ -31,61 +58,119 @@ int main(int argc, char* argv[])
          * the command and the array must have a NULL pointer
          * after the last argument.
          */
-        commandArgs[0] = strtok(command, " ");
- 
-        int i = 0;
-
-        while (commandArgs[i] != NULL)
+        if (strlen(command))
         {
-            commandArgs[i + 1] = strtok(NULL, " ");
-            i++;
-        }
 
+            commandArgs[0] = strtok(command, " ");
+     
+            int i = 0;
 
+            while (commandArgs[i] != NULL)
+            {
+                commandArgs[i + 1] = strtok(NULL, " ");
+                i++;
+            }
 
-        /* If the user entered 'exit' then call the exit() system call
-         * to terminate the process
-         */
-        if(!strcmp(commandArgs[0], "exit"))
-        {
-            exit(EXIT_SUCCESS);
-        }
-        
-        /* Fork a child process to execute the command and return 
-         * the result of the fork() in the childPid variable so 
-         * we know whether we're now executing as the parent 
-         * or child and whether or not the fork() succeeded
-         */
-        childPid = fork();
-       
-        /* TODO: Implement the cd command, allow for & */
+            // See if the command is a built-in function
 
-        if (!childPid) /* We forked no child, we ARE the child */
-        {
-            /* We're now executing in the context of the child process.
-             * Use execvp() or execlp() to replace this program in 
-             * the process' memory with the executable command the 
-             * user has asked for.  
+            /* If the user entered 'exit' then call the exit() system call
+             * to terminate the process
              */
-            // Check to see if the command exists as a shell command already
-            execvp(commandArgs[0], commandArgs); 
 
-            // Check to see if there is an ampersand appended to the command
+
+            if(!strcmp(commandArgs[0], "exit"))
+            {
+                exit(EXIT_SUCCESS);
+            }
+
+            // If the command is a cd, then this is a built-in command
+            else if (!strcmp(commandArgs[0], "cd"))
+            {
+                // Syntax is:
+                // cd [option] [directory]
+                // Needs to support absolute and relative directory
+                // PWD environement variable is present working directory
+
+                // Before we change directories, keep track of the current direcotry
+                getcwd(currentDirectory, MAX_SIZE);
+
+                // If the folder to change to is a ~, change to the home directory
+                if (!strcmp(commandArgs[1], "~"))
+                {
+                    setenv("OLDPWD", currentDirectory, OVERWRITE);
+                    
+                    // Change value of current working directory
+                    chdir(getenv("HOME"));
+                }
+
+
+                // If the folder to change to is -, then go back to the previous directory
+                if (!strcmp(commandArgs[1], "-"))
+                {
+
+                    strcpy(previousDirectory, getenv("OLDPWD"));
+
+                    setenv("OLDPWD", currentDirectory, OVERWRITE);
+
+                    chdir(previousDirectory);
+                }
+                else
+                {
+                    setenv("OLDPWD", currentDirectory, OVERWRITE);
+
+                    // Change value of current working directory
+                    chdir(commandArgs[1]);
+                }
+
             
-        }
-        else if (childPid == -1) 
-        {
-            /* An error occured during the fork - print it */
-            printf("An error occured during the process: %d", childPid);
+            }
 
-        }
-        else /* childPid is the PID of the child */
-        {
-            /* We're still executing in the parent process.
-             * Wait for the child to finish before we prompt
-             * again.
-             */
-            wait(&status);
+            else
+            {
+
+                /* Fork a child process to execute the command and return 
+                 * the result of the fork() in the childPid variable so 
+                 * we know whether we're now executing as the parent 
+                 * or child and whether or not the fork() succeeded
+                 */
+                childPid = fork();
+
+                /* TODO: Implement the cd command, allow for & */
+
+                if (!childPid) /* We forked no child, we ARE the child */
+                {
+                    /* We're now executing in the context of the child process.
+                     * Use execvp() or execlp() to replace this program in 
+                     * the process' memory with the executable command the 
+                     * user has asked for.  
+                     */
+
+                    execvp(commandArgs[0], commandArgs); 
+
+                }
+                else if (childPid == -1) 
+                {
+                    /* An error occured during the fork - print it */
+                    printf("An error occured during the process: %d", childPid);
+
+                }
+                else /* childPid is the PID of the child */
+                {
+                    /* We're still executing in the parent process.
+                     * Wait for the child to finish before we prompt
+                     * again.
+                     */
+                    
+                    // if (backgroundCommand)
+                    // {
+                    //     wait(&status);
+                    // }
+                    wait(&status);
+                }
+            }
+
+            
+
         }
 
         
