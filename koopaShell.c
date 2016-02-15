62091a79ecb8b8acade0559c29e9701e381f60de
Keiko Fujii <kfujii12@georgefox.edu>
2016-02-14 17:52:49 -0800

    Comments added

--- koopaShell.c
+++ koopaShell.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <signal.h>
 
 #define MAX_SIZE 1024
 #define MAX_COMMAND_ARGS 256
@@ -16,7 +17,6 @@
 
 int main(int argc, char* argv[])
 {
-    //TODO: Errors
     /* Variable declarations */
     char command[MAX_SIZE];
     char* commandArgs[MAX_COMMAND_ARGS]; /* Max number */
@@ -28,10 +28,13 @@ int main(int argc, char* argv[])
     // Assume that there is no background command for now
     int backgroundCommand = NO_BACKGROUND_COMMAND;
 
+    // Set the previous directory to the current one, since 
+    // we just started the shell and have no 'previous directory'
     getcwd(previousDirectory, MAX_SIZE);
 
     while(1) /* Repeat forever */
     {
+        // Get the current working directory
         getcwd(currentDirectory, MAX_SIZE);
 
         /* Display a prompt */
@@ -47,6 +50,7 @@ int main(int argc, char* argv[])
         // running in the background
         if(!strcmp(&command[strlen(command) - 1], "&"))
         {
+            // Set the background command flag
             backgroundCommand = BACKGROUND_COMMAND;
 
             // We know the command is meant to run in the background
@@ -65,78 +69,78 @@ int main(int argc, char* argv[])
          */
         if (strlen(command))
         {
-
+            // Start tokenizing the command string
             commandArgs[COMMAND] = strtok(command, " ");
      
             int i = 0;
 
+            // Tokenize the command string until we get to a 
+            // null terminator
             while (commandArgs[i] != NULL)
             {
                 commandArgs[i + 1] = strtok(NULL, " ");
                 i++;
             }
 
-            // See if the command is a built-in function
-
             /* If the user entered 'exit' then call the exit() system call
              * to terminate the process
              */
-
-
             if(!strcmp(commandArgs[COMMAND], "exit"))
             {
                 exit(EXIT_SUCCESS);
             }
 
-            // If the command is a cd, then this is a built-in command
+            /* If the command is a cd, then this is a built-in command.
+             * The current functionality implements the 'cd', 'cd -', 
+             * 'cd ~', and relative paths
+             */
             else if (!strcmp(commandArgs[COMMAND], "cd"))
             {
-                // Syntax is:
-                // cd [option] [directory]
-                // Needs to support absolute and relative directory
-                // PWD environement variable is present working directory
-
-
-
-                // Before we change directories, keep track of the current direcotry
+                // Before we change directories, keep track of the current 
+                // direcotry
                 getcwd(currentDirectory, MAX_SIZE);
 
+                /* If the command was 'cd' with no arguments, then toggle
+                 * the current directory to be the previous directory
+                 */ 
                 if (!commandArgs[FIRST_COMMAND_ARG])
                 {
+                    // Get the previous directory
                     strcpy(previousDirectory, getenv("OLDPWD"));
 
+                    // Set the new previous directory
                     setenv("OLDPWD", currentDirectory, OVERWRITE);
 
+                    // Set the current directory to be the previous directory
                     chdir(previousDirectory);
                 }
 
-                // If the folder to change to is a ~, change to the home directory
+                // If the folder to change to is a ~, change to the home 
+                // directory
                 else if (!strcmp(commandArgs[FIRST_COMMAND_ARG], "~"))
                 {
+                    // Set the new previous directory
                     setenv("OLDPWD", currentDirectory, OVERWRITE);
                     
-                    // Change value of current working directory
+                    // Change value of current working directory to home
                     chdir(getenv("HOME"));
                 }
 
-
-                // If the folder to change to is -, then go back to the previous directory
+                // If the folder to change to is -, then go back to the 
+                // previous directory and print the new current directory
                 else if (!strcmp(commandArgs[FIRST_COMMAND_ARG], "-"))
                 {
-
+                    // Set the previous directory
                     strcpy(previousDirectory, getenv("OLDPWD"));
 
+                    // Set the new previous directory
                     setenv("OLDPWD", currentDirectory, OVERWRITE);
 
+                    // Set the current directory to be the previous directory
                     chdir(previousDirectory);
 
-                    // If the first argument was a -, then print the 
-                    // new directory
-                    if (!strcmp(commandArgs[FIRST_COMMAND_ARG], "-"))
-                    {
-                        // print the new directory 
-                        printf("%s\n", previousDirectory);
-                    }
+                    // print the new directory 
+                    printf("%s\n", previousDirectory);
                 }
                 else
                 {
@@ -167,8 +171,6 @@ int main(int argc, char* argv[])
                  */
                 childPid = fork();
 
-                /* TODO: Implement the cd command, allow for & */
-
                 if (!childPid) /* We forked no child, we ARE the child */
                 {
                     /* We're now executing in the context of the child process.
@@ -179,11 +181,24 @@ int main(int argc, char* argv[])
 
                     execvp(commandArgs[COMMAND], commandArgs); 
 
+                    // Kill the child if it is unsuccessful in 
+                    // executing its duties.
+                    // Yeah, it's a bit harsh. 
+                    if (errno) 
+                    {
+                        // Print error message
+                        printf("%s\n", strerror(errno));
+
+                        // Kill the child
+                        exit(EXIT_SUCCESS);
+                    }
+
                 }
                 else if (childPid == -1) 
                 {
+
                     /* An error occured during the fork - print it */
-                    printf("An error occured during the process: %d", childPid);
+                    printf("%s\n", strerror(errno));
 
                 }
                 else /* childPid is the PID of the child */
@@ -195,20 +210,22 @@ int main(int argc, char* argv[])
                     
                     if (backgroundCommand)
                     {
+                        printf("Job %d\n", childPid);
                         waitpid(childPid, &status, 0);
+
+                        // After we are done waiting, then 
+                        // reset the background command flag
+                        backgroundCommand = NO_BACKGROUND_COMMAND;
                     }
                     else
                     {
+                        // Wait for the child to finish before we prompt again
                         wait(&status);
                     }
                 }
             }
 
-            
-
         }
-
-        
 
     } /* while */
 
